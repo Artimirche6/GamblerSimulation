0c65ec23c810b28ff9b17173ad709ca3926946c9
Artimirche6 <artimirche6@gmail.com>
2021-10-20 10:20:41 +0530

    [arti]: Gambler Simulator Program

---
+++ com/gamblerWorkshop/GamblerSimulator.java
@@ -0,0 +1,17 @@
+public class GamblerSimulator
+{
+	public GamblerSimulator()
+	{
+		int stake = 100;	//stake value per game
+		int bet = 1;	// bet per game
+		
+	}
+	
+	public static void main(String[] args)
+	{	
+		System.out.println("Gambler Simulator Program");
+		
+		GamblerSimulator gambler = new GamblerSimulator();	//Default Constructor
+		
+	}
+}
